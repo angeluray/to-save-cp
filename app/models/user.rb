e81eb2f4a54b195e841e5186c31d0eb10779cd65
angeluray <aeucampos@gmail.com>
2022-12-18 00:01:34 -0400

    Add authentication (Pending auth conditions)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         
+         Roles = [ :admin , :default ]
+
+         def is?( requested_role )
+           self.role == requested_role.to_s
+         end
 end
