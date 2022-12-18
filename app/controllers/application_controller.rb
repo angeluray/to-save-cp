86bf2495956cdba4b5f0f4045c5d256876e1ce2e
angeluray <aeucampos@gmail.com>
2022-12-17 23:31:34 -0400

    Add devise configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+        default_parameters = %i[name role]
+        devise_parameter_sanitizer.permit(:sign_up, keys: default_parameters)
+        devise_parameter_sanitizer.permit(:sign_in, keys: default_parameters)
+        devise_parameter_sanitizer.permit(:account_update, keys: default_parameters)
+      end
 end
